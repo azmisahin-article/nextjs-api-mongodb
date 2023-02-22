23cc2e87fd7a19fa231c44fd29981ecf852c6057
Azmi ŞAHİN <azmisahin@outlook.com>
2023-02-22 21:50:27 +0300

    add a city test api

---
+++ pages/api/city/index.js
@@ -0,0 +1,12 @@
+export default function handler(req, res) {
+    // get all
+    let results = ["TEST"];
+
+    // reponse result
+    if (!results) res
+        .status(404)
+        .send("Not found");
+    else res
+        .status(200)
+        .send(results);
+}
