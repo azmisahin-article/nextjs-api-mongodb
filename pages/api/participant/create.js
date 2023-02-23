b913b2d0a5009c6690250ec410436c92ca0c062e
Azmi ŞAHİN <azmisahin@outlook.com>
2023-02-23 11:12:29 +0300

    add participant create  api

---
+++ pages/api/participant/create.js
@@ -0,0 +1,23 @@
+import { createParticipant } from "@/lib/controller";
+
+export default async function handler(req, res) {
+
+    // Get data submitted in request's body.
+    const body = req.body
+
+    // Guard clause checks
+    if (!body) res
+        .status(400)
+        .send("Bad request")
+
+    // logic
+    let results = await createParticipant(body);
+
+    // reponse result
+    if (!results) res
+        .status(404)
+        .send("Not found");
+    else res
+        .status(200)
+        .send(results);
+}
