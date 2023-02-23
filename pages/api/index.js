59d08fffd0a7aff01375ac528ae6ebebfc4debbe
Azmi ŞAHİN <azmisahin@outlook.com>
2023-02-23 11:16:10 +0300

    Overview of requests and volunteer participation

---
+++ pages/api/index.js
@@ -0,0 +1,20 @@
+import { getRVP } from "@/lib/controller";
+
+/**
+ * Overview of requests and volunteer participation [RVP]
+ * @param {*} req 
+ * @param {*} res 
+ */
+export default async function handler(req, res) {
+
+    // logic
+    let results = await getRVP();
+
+    // reponse result
+    if (!results) res
+        .status(404)
+        .send("Not found");
+    else res
+        .status(200)
+        .send(results);
+}
