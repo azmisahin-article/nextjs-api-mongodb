10e98992a2629f75050abcb015146ebe32e2346f
Azmi ŞAHİN <azmisahin@users.noreply.github.com>
2023-02-23 22:10:03 +0000

    geolocation api

---
+++ pages/api/geolocation/create.js
@@ -0,0 +1,33 @@
+import { createGeolocation } from "@/lib/controller";
+import { runMiddleware } from "../cors";
+
+/**
+ * Post
+ * geolocation
+ * @param {*} req 
+ * @param {*} res 
+ */
+export default async function handler(req, res) {
+ 
+    // 
+    await runMiddleware(req, res)
+
+    // Get data submitted in request's body.
+    const body = req.body
+
+    // Guard clause checks
+    if (!body) res
+        .status(400)
+        .send("Bad request")
+
+    // logic
+    let results = await createGeolocation(body);
+
+    // reponse result
+    if (!results) res
+        .status(404)
+        .send("Not found");
+    else res
+        .status(200)
+        .send(results);
+}
