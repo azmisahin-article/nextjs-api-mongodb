449cc9a4bfbe0f82364a485814efbf1e4946c64b
Azmi ŞAHİN <azmisahin@users.noreply.github.com>
2023-02-24 08:16:03 +0000

    navigator endpoint

---
+++ pages/api/navigator/create.js
@@ -0,0 +1,41 @@
+import { createNavigator } from "@/lib/controller";
+import { runMiddleware } from "../cors";
+import requestIp from "request-ip"
+
+export async function getIpAddress(req) {
+    return await requestIp.getClientIp(req);
+}
+
+/**
+ * Post
+ * navigator
+ * @param {*} req 
+ * @param {*} res 
+ */
+export default async function handler(req, res) {
+
+    // 
+    await runMiddleware(req, res)
+
+    // Get data submitted in request's body.
+    const body = req.body
+
+    // Guard clause checks
+    if (!body) res
+        .status(400)
+        .send("Bad request")
+
+    // add ip
+    body.ip = await getIpAddress(req)
+
+    // logic
+    let results = await createNavigator(body);
+
+    // reponse result
+    if (!results) res
+        .status(404)
+        .send("Not found");
+    else res
+        .status(200)
+        .send(results);
+}
