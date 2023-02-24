af12e05f5ce9b0a694ed982def101f6115884444
Azmi ŞAHİN <azmisahin@users.noreply.github.com>
2023-02-24 08:28:37 +0000

    visitor end point

---
+++ pages/api/visitor/index.js
@@ -0,0 +1,25 @@
+import { getVisitor } from "@/lib/controller";
+import { runMiddleware } from "../cors";
+
+/**
+ * Get
+ * geolocation
+ * @param {*} req 
+ * @param {*} res 
+ */
+export default async function handler(req, res) {
+   
+    // 
+    await runMiddleware(req, res)
+
+    // logic
+    let results = await getVisitor();
+
+    // reponse result
+    if (!results) res
+        .status(404)
+        .send("Not found");
+    else res
+        .status(200)
+        .json(results);
+}
